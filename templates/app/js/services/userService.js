c8cd869252fd71c5cda8aa626244b5894f14c64e
momchilDishev <mo.dishev@gmail.com>
2015-01-10 20:34:53 +0200

    styles added to navbar and input fields

--- templates/app/js/services/userService.js
+++ templates/app/js/services/userService.js
@@ -23,10 +23,20 @@ app.factory('userService',
                 $http(request).success(success).error(error);
             },
 
+            getWaitingApprovalAds: function (params, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/user/ads?status=waitingapproval',
+                    headers: authService.getAuthHeaders(),
+                    params: params
+                };
+                $http(request).success(success).error(error);
+            },
+
             deactivateAd: function (id, success, error) {
                 var request = {
                     method: 'PUT',
-                    url: baseServiceUrl + '/api/user/ads/deactivate/'+id,
+                    url: baseServiceUrl + '/api/user/ads/deactivate/' + id,
                     headers: authService.getAuthHeaders()
                 };
                 $http(request).success(success).error(error);
@@ -35,7 +45,7 @@ app.factory('userService',
             publishAgainAd: function (id, success, error) {
                 var request = {
                     method: 'PUT',
-                    url: baseServiceUrl + '/api/user/ads/publishagain/'+id,
+                    url: baseServiceUrl + '/api/user/ads/publishagain/' + id,
                     headers: authService.getAuthHeaders()
                 };
                 $http(request).success(success).error(error);
